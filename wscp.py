2c6ba22e9955a7d3000bcd04d7a0336c986296d9
Sachu Sanjayan <sachs888@gmail.com>
2020-04-16 12:33:41 +0200

    new update clean

--- wscp.py
+++ wscp.py
@@ -8,24 +8,11 @@ from scipy.optimize import curve_fit
 from scipy.special import factorial
 from scipy.stats import poisson
 
-def gauss_function(x, a, x0, sigma):
-    return a*np.exp(-(x-x0)**2/(2*sigma**2))
-
-def bay_prob(df):
-    df.fillna(0)
-    total=df['TotalCases']
-    active=df['ActiveCases']
-    new=df['NewCases']
-    active_total=active/total
-    new_active=new/active
-    prob = new_active/active_total
-    df['prob']=prob
-    print(df['Country,Other','prob'])
-
 dtime=datetime.datetime.now()
 #res = requests.get("https://www.worldometers.info/coronavirus/")
 res = requests.get("https://www.worldometers.info/coronavirus/coronavirus-death-toll/")
 #res = requests.get("https://www.worldometers.info/coronavirus/coronavirus-cases/")
+
 soup = BeautifulSoup(res.content,'lxml')
 table = soup.find_all('table')[0] 
 df = pd.read_html(str(table))[0]
